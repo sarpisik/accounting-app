4e85c977d01fe29cb68009abbeddcd6eee7762df
Sarp ISIK <wdsarpisik@gmail.com>
2020-10-06 12:15:11 +0200

    SS configure development environment

--- packages/server/src/Server.ts
+++ packages/server/src/Server.ts
@@ -4,24 +4,22 @@ import path from 'path';
 import helmet from 'helmet';
 
 import express, { Request, Response, NextFunction } from 'express';
-import { BAD_REQUEST } from 'http-status-codes';
+
 import 'express-async-errors';
 
 import BaseRouter from './routes';
 import logger from '@shared/Logger';
-
+import { StatusCodes } from 'http-status-codes';
 
 // Init express
 const app = express();
-
-
 
 /************************************************************************************
  *                              Set basic express settings
  ***********************************************************************************/
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Show routes called in console during development
@@ -40,23 +38,9 @@ app.use('/api', BaseRouter);
 // Print API errors
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     logger.error(err.message, err);
-    return res.status(BAD_REQUEST).json({
+    return res.status(StatusCodes.BAD_REQUEST).json({
         error: err.message,
     });
-});
-
-
-
-/************************************************************************************
- *                              Serve front-end content
- ***********************************************************************************/
-
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
 });
 
 // Export express instance
