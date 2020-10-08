41013ec7e4f1386c85160b4e0a0943776c2e12cc
Sarp ISIK <wdsarpisik@gmail.com>
2020-10-08 12:49:47 +0200

    CS fix cms config

--- packages/web/gatsby-config.js
+++ packages/web/gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
             'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
     },
     plugins: [
-		'gatsby-plugin-typescript',
+        'gatsby-plugin-typescript',
         'gatsby-plugin-react-helmet',
         'gatsby-plugin-sass',
         {
@@ -79,5 +79,7 @@ module.exports = {
         }, // must be after other CSS plugins
         'gatsby-plugin-netlify', // make sure to keep it last in the array
     ],
-    proxy: [{ prefix: '/api', url: process.env.API_URL }],
+    proxy: [
+        { prefix: '/api', url: process.env.API_URL || 'http://localhost:4000' },
+    ],
 };
