a608b27d0c148ccaa003b4a573db17ccdbe6f87c
Sarp ISIK <wdsarpisik@gmail.com>
2020-10-09 17:50:34 +0200

    Remove dep gatsby-plugin-netlify

--- packages/web/gatsby-config.js
+++ packages/web/gatsby-config.js
@@ -77,7 +77,6 @@ module.exports = {
                 purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
             },
         }, // must be after other CSS plugins
-        'gatsby-plugin-netlify', // make sure to keep it last in the array
     ],
     proxy: [
         { prefix: '/api', url: process.env.API_URL || 'http://localhost:4000' },
